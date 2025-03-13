32843d16e4cf76c819740d4c1f73f51a1afac43a
AsilbekAbdusattorov <abdusattorovasilbek278@gmail.com>
2025-03-13 16:01:28 +0500

    Add

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Img1 from '../img/header/header-1.png'
 
 const Header = () => {
@@ -6,7 +7,7 @@ const Header = () => {
     <div className="bg-transparent">
       <div className="flex justify-between items-center">
         <div className="flex justify-center rounded-3xl py-[7px] px-3 bg-[#274fbb] border">
-          <button className="text-white text-center">X Закрыть</button>
+          <Link to="/register" className="text-white text-center">X Закрыть</Link>
         </div>
         <div className="bg-white w-[66px] h-[32.2px] flex justify-center items-center rounded-[5.63px] drop-shadow-[3.22px_4.02px_0px_#FF6108]">
           <div className="flex justify-between items-center">
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,18 +1,22 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
-import Footer from '../components/Footer'
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Footer from "../components/Footer";
 
 const MainLayout = () => {
   return (
-    <div className='flex flex-col w-full min-h-screen'>
-      <main className='bg-main-bg bg-cover bg-center grow py-3'>
+    <div className="flex flex-col w-full min-h-screen">
+      {/* Fon rasmi optimallashtirilgan */}
+      <main
+        className="grow py-3 bg-cover bg-center"
+        style={{ backgroundImage: "url('/bg/main.png')" }}
+      >
         <div className="w-full max-w-5xl mx-auto px-5">
-          <Outlet/>
+          <Outlet />
         </div>
       </main>
-      <Footer/>
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default MainLayout
+export default MainLayout;
