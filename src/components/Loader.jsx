f802d74d6934580703adf79b2872431edb1d3942
AsilbekAbdusattorov <abdusattorovasilbek278@gmail.com>
2025-03-14 20:33:06 +0500

    Add

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,5 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import Img1 from '../img/loading/loading.png'
+import React, { useState, useEffect } from "react";
+import Img1 from "../img/loading/loading2.png"; // O'zgartiring: loading2.png faylining manzili
+import Img2 from "../img/loading/loading3.png"; // O'zgartiring: loading3.png faylining manzili
+
 const Loader = () => {
   const [progress, setProgress] = useState(0);
 
@@ -19,10 +21,21 @@ const Loader = () => {
 
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-blue-600 text-white">
-      <div className='mb-36'>
-        <img src={Img1} alt="img" />
+      {/* Yuqori qismdagi rasm */}
+      <img src={Img2} alt="Yuqori rasm" className="mb-4" />
+
+      {/* Sarlavha */}
+      <h2 className="text-2xl text-center font-bold mb-8">Забирай<br /> гарантированный приз!</h2>
+
+      {/* Pastki qismdagi rasm */}
+      <div className="mb-12">
+        <img src={Img1} alt="Pastki rasm" />
       </div>
-      <p className="mb-2">Loading...</p>
+
+      {/* Loading yozuvi */}
+      <p className="mb-4 text-lg">Loading...</p>
+
+      {/* Progress bar */}
       <div className="w-64 h-6 bg-gray-300 rounded-full overflow-hidden">
         <div
           className="h-full bg-orange-500 transition-all"
