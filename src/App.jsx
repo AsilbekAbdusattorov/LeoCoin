e2ceb79156e51ddbca62b9b4425401e8fe937bd3
AsilbekAbdusattorov <abdusattorovasilbek278@gmail.com>
2025-03-14 22:23:34 +0500

    Add

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import {
 // Sahifalar
 import Home from "./pages/Home";
 import Tasks from "./pages/Tasks";
-import Balance from "./pages/Products";
+import Balance from "./pages/Balance";
 import PageNotFound from "./pages/PageNotFound";
 import MainLayout from "./layouts/MainLayout";
 import Invite from "./pages/Invite";
