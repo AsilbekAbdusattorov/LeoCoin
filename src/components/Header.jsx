0acd7fd64a9bd251957d46076e4337dd68aeb234
AsilbekAbdusattorov <abdusattorovasilbek278@gmail.com>
2025-03-16 11:53:17 +0500

    Add

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,17 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import Img1 from '../img/header/header-1.png'
+import Img2 from '../img/header/header2.png'
 
 const Header = ({ level }) => {
   return (
-    <div className="bg-transparent">
+    <div className="bg-transparent flex justify-center">
       <div className="flex justify-between items-center">
+        <div className="bg-white w-[66px] h-[32.2px] flex justify-center items-center rounded-[5.63px] drop-shadow-[3.22px_4.02px_0px_#FF6108]">
+          <div className="flex justify-between items-center">
+            <img className="w-[25px] h-[25px]" src={Img2} alt="logo" />
+            <h2 className="font-semibold ml-[4px]">{level}</h2>
+          </div>
+        </div>
         <div className="bg-white w-[66px] h-[32.2px] flex justify-center items-center rounded-[5.63px] drop-shadow-[3.22px_4.02px_0px_#FF6108]">
           <div className="flex justify-between items-center">
             <img className="w-[25px] h-[25px]" src={Img1} alt="logo" />
